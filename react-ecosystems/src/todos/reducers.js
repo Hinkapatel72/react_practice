afa14af3306df13af4170fe515387118e7b3d5d7
Hinka Patel <hinkapatel2994@gmail.com>
2021-07-03 22:42:56 -0700

    Todo API and add reducers

--- react-ecosystems/src/todos/reducers.js
+++ react-ecosystems/src/todos/reducers.js
@@ -1,4 +1,25 @@
-import { CREATE_TODO, REMOVE_TODO } from './actions';
+import {
+    CREATE_TODO,
+    REMOVE_TODO,
+    MARK_TODO_AS_COMPLETED,
+    LOAD_TODOS_IN_PROGRESS,
+    LOAD_TODOS_SUCCESS,
+    LOAD_TODOS_FAILURE,
+} from './actions';
+
+export const isLoading = (state = false, action) => {
+    const { type } = action;
+
+    switch (type) {
+    case LOAD_TODOS_IN_PROGRESS:
+        return true;
+    case LOAD_TODOS_SUCCESS:
+    case LOAD_TODOS_FAILURE:
+        return false;
+    default:
+        return state;
+    }
+}
 
 export const todos = (state = [], action) => {
     const { type, payload } = action;
@@ -15,6 +36,15 @@ export const todos = (state = [], action) => {
     case REMOVE_TODO: {
         const { text } = payload;
         return state.filter(todo => todo.text !== text);
+    }
+    case MARK_TODO_AS_COMPLETED: {
+        const { text } = payload;
+        return state.map(todo => {
+            if (todo.text === text) {
+                return { ...todo, isCompleted: true };
+            }
+            return todo;
+        });
     }
     default:
         return state;
