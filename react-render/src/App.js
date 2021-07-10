84303712d0e64478ca315f18754d7da92140e1c0
Hinka Patel <hinkapatel2994@gmail.com>
2021-07-10 15:05:04 -0700

    Practice React render and re-render using useState and useReducer

---
+++ react-render/src/App.js
@@ -0,0 +1,15 @@
+import './App.css';
+// import { UseState } from './components/UseState/UseState';
+import { UseReducer } from './components/UseReducer/UseReducer';
+
+
+function App() {
+  return (
+    <div className="App">
+      {/* <UseState /> */}
+      <UseReducer />
+    </div>
+  );
+}
+
+export default App;
